0b0fd36d0e0042a515a177599cc9eb053a91bacf
nqcuong720 <nqcuong720@gmail.com>
2021-06-27 16:16:25 +0700

    Implementation_Position_Services

--- CoffeeManagementSystem.Services/PositionServices/PositionServices.cs
+++ CoffeeManagementSystem.Services/PositionServices/PositionServices.cs
@@ -5,33 +5,80 @@
 namespace CoffeeManagementSystem.Services.PositionServices
 {
     using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using CoffeeManagementSystem.Model.EntitiesModel;
     using CoffeeManagementSystem.Model.Model;
+    using CoffeeManagementSystem.Model.Response;
 
     public class PositionServices : BaseServices.BaseServices, IPositionServices
     {
+        #region Contructors, Variable
+        private readonly IMapper _mapper;
+
+        public PositionServices(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        #endregion
+
+        #region Create Position
         public PositionModel CreatePosition(PositionModel posititonModel)
         {
-            throw new System.NotImplementedException();
+            return _mapper.Map<PositionModel>(InsertReturnModel(_mapper.Map<PositionEntities>(posititonModel)));
         }
+        #endregion
 
+        #region Delete Position
         public bool DeletePosition(int positionId)
         {
-            throw new System.NotImplementedException();
+            PositionEntities position = GetObject<PositionEntities>(po => po.Id == positionId);
+            if(position != null)
+            {
+                return DeleteObject<PositionEntities>(position);
+            }
+            return false;
         }
+        #endregion
 
-        public PositionModel DetailPositionById(int positionId)
+        #region DetailPositionById
+        public PositionDetailRespone DetailPositionById(int positionId)
         {
-            throw new System.NotImplementedException();
+            var position = (from po in Context.Set<PositionEntities>()
+                            from us in Context.Set<UserEntities>().Where(us => us.Id == po.CreateBy).DefaultIfEmpty()
+                            from us2 in Context.Set<UserEntities>().Where(us2 => us2.Id == po.UpdateBy).DefaultIfEmpty()
+                            where po.Id == positionId
+                            select new PositionDetailRespone
+                            {
+                                Id = po.Id,
+                                PositionName = po.PositionName,
+                                CreateBy = po.CreateBy,
+                                CreateByUser = us.LastName + " " + us.FirstName,
+                                UpdateBy = po.UpdateBy,
+                                UpdateByUser = us2.LastName + " " + us2.FirstName
+                            }).FirstOrDefault();
+            return position;            
         }
+        #endregion
 
-        public List<PositionModel> GetListAllPosition()
+        #region GetListAllPosition
+        public List<PositionModel> GetListAllPosition(int pageSize, int pageNumber)
         {
-            throw new System.NotImplementedException();
-        }
+            List<PositionModel> listPosition = _mapper.Map<List<PositionModel>>(GetList<PositionEntities>());
 
+            if (pageSize > 0)
+            {
+                listPosition = listPosition.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
+            }
+            return listPosition;
+        }
+        #endregion
+
+        #region UpdatePostion
         public PositionModel UpdatePosition(PositionModel posititonModel)
         {
-            throw new System.NotImplementedException();
+            return _mapper.Map<PositionModel>(UpdateReturnModel(_mapper.Map<PositionEntities>(posititonModel)));
         }
+        #endregion
     }
 }
